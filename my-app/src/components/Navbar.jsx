5a550fc01dede37e876c2334827db10c1074f210
Marco Pontiz <marcopontiz@gmail.com>
2022-08-29 18:52:06 -0300

    Cambiando unos detalles

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import CartWidget from "./CartWidget.jsx";
 
 const Navbar = () => {
     return(
-        <nav class="navbar navbar-expand-lg bg-warning">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Snowdin</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
