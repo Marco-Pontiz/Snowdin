c6331ebf027d82d95b2ba25f676316a8ee41c621
Marco Pontiz <marcopontiz@gmail.com>
2022-09-27 19:18:08 -0300

    agregando firebase

--- my-app/src/components/ItemDetailContainer/IndexDetail.jsx
+++ my-app/src/components/ItemDetailContainer/IndexDetail.jsx
@@ -1,32 +1,24 @@
 import React,  {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from '../ItemDetail/IndexDetail.jsx';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
-const Products = [
-    { id:1, image:"https://m.media-amazon.com/images/I/41uN5FWDE6L.jpg", title:"Backpack snow Dakine", subtitle:"Waterproof snow backpack.", price:300},
-    {id:2, image:"https://http2.mlstatic.com/D_NQ_NP_779962-MLA42372645958_062020-O.webp", title:"Snow goggles", subtitle:"Adidas brand. Made with quality material.", price:150},
-    {id:3, image:"https://www.estiber.com/blog/wp-content/uploads/2019/10/baston-esqui.jpg", title:"Snow Sticks", subtitle:"Perfect ski poles", price:440}
-];
+// const Products = [
+//     { id:1, image:"https://m.media-amazon.com/images/I/41uN5FWDE6L.jpg", title:"Backpack snow Dakine", subtitle:"Waterproof snow backpack.", price:300},
+//     {id:2, image:"https://http2.mlstatic.com/D_NQ_NP_779962-MLA42372645958_062020-O.webp", title:"Snow goggles", subtitle:"Adidas brand. Made with quality material.", price:150},
+//     {id:3, image:"https://www.estiber.com/blog/wp-content/uploads/2019/10/baston-esqui.jpg", title:"Snow Sticks", subtitle:"Perfect ski poles", price:440}
+// ];
 
 const ItemDetailContainer = () => {
     const {idProduct} = useParams()
     const [data, setData] = useState ({});
 
     useEffect(() =>{
-        const getData = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(Products.find(e => e.id == idProduct));
-            }, 3000);
-        });
-        getData.then(res => setData(res));
-
-        if(idProduct){
-            getData.then(res => setData(res.find(Prod => Prod.id === idProduct)));
-    } else {
-        getData.then(res => setData(res));
-    }
-    }, []
-    )
+        const querydb = getFirestore();
+        const queryDoc = doc(querydb, 'products', idProduct);
+        getDoc(queryDoc)
+            .then(res => setData({id: res.id, ...res.data()}));
+    },[idProduct]);
 
     return(
         <ItemDetail data={data} />
--- my-app/src/container/ItemListContainer.jsx
+++ my-app/src/container/ItemListContainer.jsx
@@ -1,26 +1,30 @@
 import ItemList from "../Item/ItemList";
 import React, {useEffect,useState} from 'react'
 import { useParams } from "react-router-dom";
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 
+/*
 const Elements = [
-    { id:1, image:"https://m.media-amazon.com/images/I/41uN5FWDE6L.jpg", title:"Backpack snow Dakine", subtitle:"Waterproof snow backpack, able to carry heavy weights for tools. Ideal for mountain climbing or camping.", price:'£300'},
-    { id:2, image:"https://http2.mlstatic.com/D_NQ_NP_779962-MLA42372645958_062020-O.webp", title:"Snow goggles", subtitle:"Adidas brand. Made with quality material.", price:'£150'},
-    { id:3, image:"https://www.estiber.com/blog/wp-content/uploads/2019/10/baston-esqui.jpg", title:"Snow Sticks", subtitle:"Perfect ski poles", price:'£440'}
+    { id:1, image:"https://m.media-amazon.com/images/I/41uN5FWDE6L.jpg", title:"Backpack snow Dakine", subtitle:"Waterproof snow backpack, able to carry heavy weights for tools. Ideal for mountain climbing or camping.", price:300},
+    { id:2, image:"https://http2.mlstatic.com/D_NQ_NP_779962-MLA42372645958_062020-O.webp", title:"Snow goggles", subtitle:"Adidas brand. Made with quality material.", price:150},
+    { id:3, image:"https://www.estiber.com/blog/wp-content/uploads/2019/10/baston-esqui.jpg", title:"Snow Sticks", subtitle:"Perfect ski poles", price:440}
 ]
-
+*/
 const ItemListContainer = (greeting) => {
 
     const [data, setData] = useState([]);
     const { idProducts } = useParams();
 
     useEffect(() =>{
-        const getData = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(Elements)
-            }, 3000);
-        }); 
-        getData.then(res => setData(res));
-    }, [])
+        const querydb = getFirestore();
+        const queryCollection = collection(querydb, 'products');
+        if (idProducts){
+            const queryFilter = query(queryCollection, where('category', '==', idProducts))
+            getDocs(queryFilter).then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))
+        } else {
+            getDocs(queryCollection).then(res => setData(res.docs.map(product => ({id: product.id, ...product.data()}))))
+        }
+    }, [idProducts]);
     console.log(greeting);
 
     return(
